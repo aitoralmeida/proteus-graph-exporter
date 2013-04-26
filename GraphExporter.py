ee2af755def95024a9d6e9aef461e3caed7509da
Aitor Almeida <aitoralmeida@gmail.com>
2013-04-26 14:19:48 +0200

    corrected some minor bugs in the gfex exporter

--- GraphExporter.py
+++ GraphExporter.py
@@ -44,7 +44,7 @@ class Graph:
             # metadata
             f.write('    <meta lastmodifieddate="' + self.modified + '">\n')
             f.write('        <creator>' + self.creator + '</creator>\n')
-            f.write('        <description>' +  + '</description>\n')
+            f.write('        <description>' + self.description + '</description>\n')
             f.write('    </meta>\n')
             
             #graph
@@ -64,11 +64,11 @@ class Graph:
             for node in self.nodes:
                 # right niow the id and label of the node are the same
                 f.write('        <node id="' + node  + '" label="'+ node +'">\n')
-                if len(self.nodes[node] > 0):
+                if len(self.nodes[node]) > 0:
                     f.write('            <attvalues>\n')
                     i = 0
                     for att in self.nodes[node]:
-                        f.write('             <attvalue for="' + i +'" value="' + att + '"/>\n')
+                        f.write('             <attvalue for="' + str(i) +'" value="' + att + '"/>\n')
                         i += 1
                     f.write('            </attvalues>\n')
                 f.write('        </node>\n')            
@@ -79,7 +79,7 @@ class Graph:
             f.write('        </edges>\n')
             i = 0
             for edge in self.edges:
-                f.write('            <edge id="' + i + '" source="' +  edge[0] + '" target="' + edge[1] + '"/>\n')
+                f.write('            <edge id="' + str(i) + '" source="' +  edge[0] + '" target="' + edge[1] + '"/>\n')
                 i += 1
                 
             
@@ -110,4 +110,5 @@ if __name__ == "__main__":
     g.export_graph_ncol('./files/graphNCOL.txt')
     print 'Exporting GEFX'
     g.export_graph_gefx('./files/graphGEFX.gefx')    
+    print 'done'
     
