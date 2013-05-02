67727554b39f322a94f11679184d0ebf4f1527f1
Aitor Almeida <aitoralmeida@gmail.com>
2013-05-02 11:09:52 +0300

    Update setup.py
    added categories and status

--- setup.py
+++ setup.py
@@ -5,22 +5,21 @@ from setuptools import setup
 
 # TODO: select categories
 classifiers=[
-    "Development Status :: 3 - Alpha",
+    "Development Status :: 4 - Beta",
     "Environment :: Web Environment",
     "Intended Audience :: Developers",
     "License :: Freely Distributable",
     "Operating System :: OS Independent",
     "Programming Language :: Python",
     "Programming Language :: Python :: 2",
-    "Topic :: Internet :: WWW/HTTP",
-    "Topic :: Software Development :: Libraries :: Application Frameworks",
+    "Topic :: Software Development :: Libraries",
 ]
 
 cp_license="MIT"
 
 setup(name='graph-exporter',
-      version='0.1',
-      description="A  library to export graphs to different formats",
+      version='0.2',
+      description="A  library to export network graphs to different formats",
       classifiers=classifiers,
       author='Aitor Almeida',
       author_email='aitor.almeida@deusto.es',
